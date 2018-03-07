57117c84019dc44c8b0d2985b40a45d07f50fa7e
Nick Harding <njh@well.ox.ac.uk>
2018-03-07 11:03:06 +0000

    add perl hack

--- makedocs.sh
+++ makedocs.sh
@@ -3,4 +3,6 @@ set -e
 cd docs/ && make clean && make html 
 cd ..
 touch docs/_build/html/.nojekyll
+perl -lape '$_ =~ s/\/scripts\/bokeh/scripts\/bokeh/;' index.html > q.html
+mv q.html index.html
 ./deploy.sh
