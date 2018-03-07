a04f5eb007382b9832853f86f28f33215a44b951
Nick Harding <njh@well.ox.ac.uk>
2018-03-07 11:04:52 +0000

    fix paths

--- makedocs.sh
+++ makedocs.sh
@@ -3,6 +3,5 @@ set -e
 cd docs/ && make clean && make html 
 cd ..
 touch docs/_build/html/.nojekyll
-perl -lape '$_ =~ s/\/scripts\/bokeh/scripts\/bokeh/;' index.html > q.html
-mv q.html index.html
+cd docs/_build/html && perl -lape '$_ =~ s/\/scripts\/bokeh/scripts\/bokeh/;' index.html > q.html && mv q.html index.html
 ./deploy.sh
